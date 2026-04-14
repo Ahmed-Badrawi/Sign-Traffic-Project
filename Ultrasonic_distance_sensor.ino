64c1286391a98a7ae0fdaf50712d4060df366fa4
Ahmed Badrawi <ahmed.badrawi@outlook.de>
2026-04-14 12:51:11 +0200

    Create Ultrasonic_distance_sensor.ino

---
+++ Ultrasonic_distance_sensor.ino
@@ -0,0 +1,24 @@
+int trigPin = 9; //PWM
+int ecoPin = 10; // PWM
+long duration;
+int distance;
+
+void setup() {
+  pinMode (trigPin, OUTPUT);
+  pinMode (ecoPin, INPUT);
+  Serial.begin (9600); // Serial Communication 
+
+}
+
+void loop() {
+  digitalWrite (trigPin, LOW);
+  delayMicroseconds (2);
+  digitalWrite (trigPin, HIGH);
+  delayMicroseconds (10);
+  digitalWrite (trigPin, LOW); // to send new signals
+  duration = pulseIn (ecoPin, HIGH); // Time
+  distance = duration * 0.034/2;
+  Serial.print ("Distance: ");
+  Serial.println (distance);
+
+}
